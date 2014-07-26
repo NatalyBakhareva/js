7103492e1de695b31e2d62f6aaa9de6bb69dbfac
nadirq <nadirq.ru@gmail.com>
2014-07-26 14:02:40 +0700

    Homework. Done

---
+++ action.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "Hello, <strong>". $_POST['fio'] ."</strong>!<br />";
+echo "Your login: <strong>". $_POST['login'] ."</strong><br />";
+echo "Your password: <strong>". $_POST['pass'] ."</strong><br />";
+echo "About you: <strong>". $_POST['about'] ."</strong><br />";
